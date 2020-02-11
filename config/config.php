58ab018f95bae8d44ea07efd96760ef754f3530c
Florian Wartner <fwartner@users.noreply.github.com>
2020-02-11 12:05:55 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,5 +3,5 @@
 return [
     'xentral_host' => env('XENTRAL_HOST', null),
     'xentral_api_username' => env('XENTRAL_API_USERNAME', null),
-    'xentral_api_key' => env('XENTRAL_API_USERNAME', null),
+    'xentral_api_key' => env('XENTRAL_API_KEY', null),
 ];
