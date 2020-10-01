594bfab2800f6e1fc27f2c29bc82fa430ec345be
Florian Wartner <florian@wartner.io>
2020-10-01 11:10:17 +0200

    WIP

--- src/Models/Address.php
+++ src/Models/Address.php
@@ -41,4 +41,39 @@ class Address
     {
         return json_decode($this->client->get($this->url . '/adresstyp/' . $id)->getBody()->getContents(), true);
     }
+
+    /**
+     * @param $type
+     * @param $language
+     * @param $name
+     * @param $country
+     * @param $street
+     * @param $city
+     * @param $zipcode
+     * @param $telephone
+     * @param $telefax
+     * @param $email
+     * @param $projectId
+     * @return \Psr\Http\Message\StreamInterface
+     */
+    public function createAddress($type, $language, $name, $country, $street, $city, $zipcode, $telephone, $telefax, $email, $projectId)
+    {
+        $data = [
+            'typ' => $type ?? 'person',
+            'sprache' => $language ?? 'deutsch',
+            'name' => $name,
+            'land' => $country ?? 'DE',
+            'strasse' => $street,
+            'ort' => $city,
+            'plz' => $zipcode,
+            'telefon' => $telephone,
+            'telefax' => $telefax,
+            'email' => $email,
+            'projekt' => $projectId
+        ];
+
+        $response = $this->client->post($this->url . '/adressen/', ['data' => $data]);
+
+        return $response->getBody();
+    }
 }
