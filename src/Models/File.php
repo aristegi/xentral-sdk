7d711820f1179d0256555979b4a7f0a454eb0d92
Florian Wartner <florian@wartner.io>
2020-10-19 11:50:27 +0200

    Updates

--- src/Models/File.php
+++ src/Models/File.php
@@ -29,4 +29,14 @@ class File
     {
         return json_decode($this->client->get($this->url . '/dateien?dateiname_startswith=' . $filename)->getBody()->getContents(), true);
     }
+
+    public function downloadFile($id)
+    {
+        return json_decode($this->client->get($this->url . '/dateien/' . $id . '/download')->getBody()->getContents(), true);
+    }
+
+    public function getBase64($id)
+    {
+        return json_decode($this->client->get($this->url . '/dateien/' . $id . '/base64')->getBody()->getContents(), true);
+    }
 }
